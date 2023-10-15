c9f49b6552e7fb1c0500bc879395801cc102400e
Ilya <ilya.nevolin.2004@mail.ru>
2023-10-15 22:36:17 +0500

    Создал пробную таблицы, но без данных

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -16,7 +16,7 @@ namespace Bank
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login_form());
+            Application.Run(new Test_table());
         }
     }
 }

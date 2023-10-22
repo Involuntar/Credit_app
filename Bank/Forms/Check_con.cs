c5f068b933d1219ca7e332b236301e09dc7c618b
Involuntar <ilya.nevolin.2004@mail.ru>
2023-10-22 21:54:20 +0500

    Revert "удалил папку с формами"
    This reverts commit c21997c4a19aaf519fe9e7a829e4efd6ce2892c3.

---
+++ Bank/Forms/Check_con.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank
+{
+
+    public partial class Check_conn : Form
+    {
+        Data.Connection con = new Data.Connection();
+        public Check_conn()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Data.Connection.dataSource();1
+                con.connOpen();
+                Success.Visible = true;
+                Error.Visible = false;
+                con.connClose();
+            }
+            catch (Exception)
+            {
+                Error.Visible = true;
+                Success.Visible = false;
+                con.connClose();
+            }
+            finally
+            {
+                con.connClose();
+            }
+        }*/
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bank/Forms/Login_form.cs
@@ -0,0 +1,105 @@
+﻿using Bank.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank
+{
+    public partial class Login_form : Form
+    {
+        public Login_form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_form_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Log_in_Click(object sender, EventArgs e)
+        {
+            if (User.Checked == true)
+            {
+                string login_check = Login.Text;
+                string password_check = Password.Text;
+                MySqlConnection con = Connection.GetConnection();
+                string sql = $"SELECT login, password FROM users " +
+                    $"WHERE login LIKE @login AND password LIKE @password";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.Add("@login", MySqlDbType.VarChar).Value = login_check;
+                cmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = password_check;
+
+                try
+                {
+                    object pass_check = cmd.ExecuteScalar();
+                    if (pass_check != null)
+                    {
+                        MessageBox.Show("Login successful", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Hide();
+                        Product_choice pr_ch = new Product_choice();
+                        pr_ch.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login denied", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            else if (Admin.Checked == true)
+            {
+                string login_check = Login.Text;
+                string password_check = Password.Text;
+                MySqlConnection con = Connection.GetConnection();
+                string sql = $"SELECT login, password FROM admins " +
+                    $"WHERE login LIKE @login AND password LIKE @password";
+                MySqlCommand cmd = new MySqlCommand(sql, con);
+                cmd.Parameters.Add("@login", MySqlDbType.VarChar).Value = login_check;
+                cmd.Parameters.Add("@password", MySqlDbType.VarChar).Value = password_check;
+
+                try
+                {
+                    object pass_check = cmd.ExecuteScalar();
+                    if (pass_check != null)
+                    {
+                        MessageBox.Show("Login successful", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Login denied", "Info", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Chose your role", "Recomendation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check_conn check = new Check_conn();
+            check.Show();
+        }
+    }
+}

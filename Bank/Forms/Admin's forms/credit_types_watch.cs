fd38f7a22389263efc64764059720c17ad1787b4
Involuntar <ilya.nevolin.2004@mail.ru>
2023-10-23 19:54:37 +0500

    Создал формы для админа

---
+++ Bank/Forms/Admin's forms/credit_types_watch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank.Forms.Admin_s_forms
+{
+    public partial class credit_types_watch : Form
+    {
+        public credit_types_watch()
+        {
+            InitializeComponent();
+        }
+    }
+}

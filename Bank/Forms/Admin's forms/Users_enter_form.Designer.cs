11bc2671333c6614554a505a88d613914902f2e0
Maria <testirovsik0005mail.ru>
2023-10-30 16:22:25 +0500

    Изменения на форме Ввод пользователей

--- Bank/Forms/Admin's forms/Users_enter_form.Designer.cs
+++ Bank/Forms/Admin's forms/Users_enter_form.Designer.cs
@@ -46,11 +46,12 @@
             // 
             // firstnametxt
             // 
-            this.firstnametxt.Location = new System.Drawing.Point(228, 89);
+            this.firstnametxt.Location = new System.Drawing.Point(58, 91);
             this.firstnametxt.Margin = new System.Windows.Forms.Padding(2);
             this.firstnametxt.Name = "firstnametxt";
             this.firstnametxt.Size = new System.Drawing.Size(76, 20);
             this.firstnametxt.TabIndex = 0;
+            this.firstnametxt.Text = "Фамилия";
             // 
             // middlenametxt
             // 
@@ -96,79 +97,79 @@
             // firstname_txt
             // 
             this.firstname_txt.AutoSize = true;
-            this.firstname_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.firstname_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.firstname_txt.Location = new System.Drawing.Point(79, 84);
+            this.firstname_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.firstname_txt.ForeColor = System.Drawing.Color.Black;
+            this.firstname_txt.Location = new System.Drawing.Point(53, 63);
             this.firstname_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.firstname_txt.Name = "firstname_txt";
-            this.firstname_txt.Size = new System.Drawing.Size(57, 23);
+            this.firstname_txt.Size = new System.Drawing.Size(48, 26);
             this.firstname_txt.TabIndex = 7;
             this.firstname_txt.Text = "Имя";
             // 
             // middlename_txt
             // 
             this.middlename_txt.AutoSize = true;
-            this.middlename_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.middlename_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.middlename_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.middlename_txt.ForeColor = System.Drawing.Color.Black;
             this.middlename_txt.Location = new System.Drawing.Point(79, 112);
             this.middlename_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.middlename_txt.Name = "middlename_txt";
-            this.middlename_txt.Size = new System.Drawing.Size(120, 23);
+            this.middlename_txt.Size = new System.Drawing.Size(100, 26);
             this.middlename_txt.TabIndex = 8;
             this.middlename_txt.Text = "Отчество";
             // 
             // lastname_txt
             // 
             this.lastname_txt.AutoSize = true;
-            this.lastname_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.lastname_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lastname_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.lastname_txt.ForeColor = System.Drawing.Color.Black;
             this.lastname_txt.Location = new System.Drawing.Point(79, 148);
             this.lastname_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lastname_txt.Name = "lastname_txt";
-            this.lastname_txt.Size = new System.Drawing.Size(106, 23);
+            this.lastname_txt.Size = new System.Drawing.Size(93, 26);
             this.lastname_txt.TabIndex = 9;
             this.lastname_txt.Text = "Фамилия";
             // 
             // login_txt
             // 
             this.login_txt.AutoSize = true;
-            this.login_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.login_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.login_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.login_txt.ForeColor = System.Drawing.Color.Black;
             this.login_txt.Location = new System.Drawing.Point(79, 183);
             this.login_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.login_txt.Name = "login_txt";
-            this.login_txt.Size = new System.Drawing.Size(74, 23);
+            this.login_txt.Size = new System.Drawing.Size(66, 26);
             this.login_txt.TabIndex = 10;
             this.login_txt.Text = "Логин";
             // 
             // password_txt
             // 
             this.password_txt.AutoSize = true;
-            this.password_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.password_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.password_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.password_txt.ForeColor = System.Drawing.Color.Black;
             this.password_txt.Location = new System.Drawing.Point(79, 216);
             this.password_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.password_txt.Name = "password_txt";
-            this.password_txt.Size = new System.Drawing.Size(87, 23);
+            this.password_txt.Size = new System.Drawing.Size(78, 26);
             this.password_txt.TabIndex = 11;
             this.password_txt.Text = "Пароль";
             // 
             // email_txt
             // 
             this.email_txt.AutoSize = true;
-            this.email_txt.Font = new System.Drawing.Font("Georgia", 14.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
-            this.email_txt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.email_txt.Font = new System.Drawing.Font("Goudy Stout", 14.25F);
+            this.email_txt.ForeColor = System.Drawing.Color.Black;
             this.email_txt.Location = new System.Drawing.Point(79, 252);
             this.email_txt.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.email_txt.Name = "email_txt";
-            this.email_txt.Size = new System.Drawing.Size(82, 23);
+            this.email_txt.Size = new System.Drawing.Size(66, 26);
             this.email_txt.TabIndex = 12;
             this.email_txt.Text = "Почта";
             // 
             // Back
             // 
             this.Back.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
-            this.Back.Font = new System.Drawing.Font("Georgia", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Back.Font = new System.Drawing.Font("Goudy Stout", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Back.ForeColor = System.Drawing.Color.White;
             this.Back.Location = new System.Drawing.Point(415, 10);
             this.Back.Margin = new System.Windows.Forms.Padding(2);
@@ -191,6 +192,8 @@
             this.Save.ButtonPressedColor2 = System.Drawing.Color.Maroon;
             this.Save.ButtonPressedForeColor = System.Drawing.Color.White;
             this.Save.ButtonRoundRadius = 30;
+            this.Save.Font = new System.Drawing.Font("Goudy Stout", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Save.ForeColor = System.Drawing.Color.White;
             this.Save.Location = new System.Drawing.Point(118, 325);
             this.Save.Name = "Save";
             this.Save.Size = new System.Drawing.Size(139, 50);

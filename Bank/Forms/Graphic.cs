be583d7311f7ad2eae23683cde3a50a56f3445cc
Ilya <ilya.nevolin.2004@mail.ru>
2023-11-28 21:55:20 +0500

    отредактировал отображение таблицы графика платежей

---
+++ Bank/Forms/Graphic.cs
@@ -0,0 +1,44 @@
+﻿using Bank.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bank.Forms
+{
+    public partial class Graphic : Form
+    {
+        public Graphic()
+        {
+            InitializeComponent();
+        }
+
+        private void Graphic_Load(object sender, EventArgs e)
+        {
+            double remain = Convert.ToDouble(public_class.Start_Summ);
+            double perc = remain * Convert.ToDouble(public_class.Rate) * 0.01 / 12;
+            double debt = remain / (Convert.ToUInt16(public_class.Term) * 12);
+            for (int i = 0; i < Convert.ToUInt16(public_class.Term) * 12; ++i)
+            {
+                int rowIndex = dataGridGraphic.Rows.Add();
+                dataGridGraphic.Rows[rowIndex].Cells["Month"].Value = i + 1;
+                dataGridGraphic.Rows[rowIndex].Cells["Remain"].Value = remain;
+                dataGridGraphic.Rows[rowIndex].Cells["Payment"].Value = perc + debt;
+                dataGridGraphic.Rows[rowIndex].Cells["Percents"].Value = perc;
+                dataGridGraphic.Rows[rowIndex].Cells["Debt"].Value = debt;
+                dataGridGraphic.Rows[rowIndex].Cells["Balance"].Value = remain - debt;
+                remain -= debt;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- Bank/Grafic_show.Designer.cs
+++ Bank/Grafic_show.Designer.cs
@@ -29,20 +29,30 @@
         private void InitializeComponent()
         {
             this.dataGridGraf = new System.Windows.Forms.DataGridView();
-            this.Graf_back = new System.Windows.Forms.Button();
             this.month = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ost_debt = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.payment = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.procent_part = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.debt_part = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.end_debt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Graf_back = new System.Windows.Forms.Button();
             this.diff_add = new Bank.RoundButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridGraf)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridGraf
             // 
-            this.dataGridGraf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridGraf.AllowUserToAddRows = false;
+            this.dataGridGraf.AllowUserToDeleteRows = false;
+            this.dataGridGraf.AllowUserToResizeColumns = false;
+            this.dataGridGraf.AllowUserToResizeRows = false;
+            this.dataGridGraf.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridGraf.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridGraf.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridGraf.ColumnHeadersHeight = 29;
+            this.dataGridGraf.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.dataGridGraf.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.month,
             this.ost_debt,
@@ -50,53 +60,66 @@
             this.procent_part,
             this.debt_part,
             this.end_debt});
-            this.dataGridGraf.Location = new System.Drawing.Point(22, 23);
+            this.dataGridGraf.Location = new System.Drawing.Point(29, 28);
+            this.dataGridGraf.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridGraf.MultiSelect = false;
             this.dataGridGraf.Name = "dataGridGraf";
-            this.dataGridGraf.Size = new System.Drawing.Size(644, 392);
+            this.dataGridGraf.RowHeadersVisible = false;
+            this.dataGridGraf.RowHeadersWidth = 51;
+            this.dataGridGraf.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridGraf.ShowEditingIcon = false;
+            this.dataGridGraf.Size = new System.Drawing.Size(859, 482);
             this.dataGridGraf.TabIndex = 0;
+            // 
+            // month
+            // 
+            this.month.HeaderText = "Месяц";
+            this.month.MinimumWidth = 6;
+            this.month.Name = "month";
+            // 
+            // ost_debt
+            // 
+            this.ost_debt.HeaderText = "Остаток долга";
+            this.ost_debt.MinimumWidth = 6;
+            this.ost_debt.Name = "ost_debt";
+            // 
+            // payment
+            // 
+            this.payment.HeaderText = "Сумма платежа";
+            this.payment.MinimumWidth = 6;
+            this.payment.Name = "payment";
+            // 
+            // procent_part
+            // 
+            this.procent_part.HeaderText = "Платеж по процентам";
+            this.procent_part.MinimumWidth = 6;
+            this.procent_part.Name = "procent_part";
+            // 
+            // debt_part
+            // 
+            this.debt_part.HeaderText = "Платеж по основному долгу";
+            this.debt_part.MinimumWidth = 6;
+            this.debt_part.Name = "debt_part";
+            // 
+            // end_debt
+            // 
+            this.end_debt.HeaderText = "Остаток долга на конец периода";
+            this.end_debt.MinimumWidth = 6;
+            this.end_debt.Name = "end_debt";
             // 
             // Graf_back
             // 
             this.Graf_back.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(84)))), ((int)(((byte)(84)))));
             this.Graf_back.Font = new System.Drawing.Font("Goudy Stout", 9F);
             this.Graf_back.ForeColor = System.Drawing.Color.White;
-            this.Graf_back.Location = new System.Drawing.Point(785, 12);
+            this.Graf_back.Location = new System.Drawing.Point(1047, 15);
+            this.Graf_back.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Graf_back.Name = "Graf_back";
-            this.Graf_back.Size = new System.Drawing.Size(81, 27);
+            this.Graf_back.Size = new System.Drawing.Size(108, 33);
             this.Graf_back.TabIndex = 1;
             this.Graf_back.Text = "Назад";
             this.Graf_back.UseVisualStyleBackColor = false;
             this.Graf_back.Click += new System.EventHandler(this.Graf_back_Click);
-            // 
-            // month
-            // 
-            this.month.HeaderText = "Месяц";
-            this.month.Name = "month";
-            // 
-            // ost_debt
-            // 
-            this.ost_debt.HeaderText = "Остаток долга";
-            this.ost_debt.Name = "ost_debt";
-            // 
-            // payment
-            // 
-            this.payment.HeaderText = "Сумма платежа";
-            this.payment.Name = "payment";
-            // 
-            // procent_part
-            // 
-            this.procent_part.HeaderText = "Платеж по процентам";
-            this.procent_part.Name = "procent_part";
-            // 
-            // debt_part
-            // 
-            this.debt_part.HeaderText = "Платеж по основному долгу";
-            this.debt_part.Name = "debt_part";
-            // 
-            // end_debt
-            // 
-            this.end_debt.HeaderText = "Остаток долга на конец периода";
-            this.end_debt.Name = "end_debt";
             // 
             // diff_add
             // 
@@ -112,22 +135,24 @@
             this.diff_add.ButtonRoundRadius = 30;
             this.diff_add.Font = new System.Drawing.Font("Goudy Stout", 9F);
             this.diff_add.ForeColor = System.Drawing.Color.White;
-            this.diff_add.Location = new System.Drawing.Point(729, 371);
+            this.diff_add.Location = new System.Drawing.Point(972, 457);
+            this.diff_add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.diff_add.Name = "diff_add";
-            this.diff_add.Size = new System.Drawing.Size(128, 44);
+            this.diff_add.Size = new System.Drawing.Size(171, 54);
             this.diff_add.TabIndex = 2;
             this.diff_add.Text = "Оставить заявку";
             this.diff_add.Click += new System.EventHandler(this.diff_add_Click);
             // 
             // Grafic_show
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
-            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.ClientSize = new System.Drawing.Size(1171, 554);
             this.Controls.Add(this.diff_add);
             this.Controls.Add(this.Graf_back);
             this.Controls.Add(this.dataGridGraf);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Grafic_show";
             this.Text = "Вывод графика";
             this.Load += new System.EventHandler(this.Grafic_show_Load);
